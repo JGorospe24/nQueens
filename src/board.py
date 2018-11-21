af977666bdeafeca2a71456141a8683f20da9158
JGorospe24 <jaenuzagorospe@gmail.com>
2018-11-21 18:50:08 -0500

    Re-did the check constraints to make it faster and more robust

--- src/board.py
+++ src/board.py
@@ -1,18 +1,13 @@
 import random
-
+import timeit
 class Board:
     def __init__(self, n):
         self.queens = dict()
         self.columns = list()
         self.initializeBoard(n)
-        self.Max = 0
-
-
+        self.Max = n - 1
 
     def initializeBoard(self, n):
-        # Subtract 1 from n to account for index starting at 0
-        self.Max = n -1
-
         for x in range(n):
             columnTiles = list()
 
@@ -22,141 +17,84 @@ class Board:
 
             #Generate a random position within a column for place a queen, update the Queen dictionary
             queenPosition = random.randint(0,n-1)
-            self.queens["Q" + str(x+1)] = queenPosition
+            self.queens["Q" + str(x)] = queenPosition
 
             #At this point, we should have a column list of all the columns and its values, and a queens dictionary to keep track of where the queens are placed within a column
             self.columns.append(columnTiles)
 
-    def checkConflictsHorizontal(self):
-        col = 0
-        #Get a new list of constraints, representing the next column
-        for conflicts in self.columns:
-            row = 0
-            #Iterate through each row in this column
-            for x in conflicts:
-                tempConflicts = 0
+    def updateConflicts(self):
 
-                #Check every column for queen in current row
-                for y in self.queens:
-                    print("Checking row {} in {} for spot {} in list {}".format(row, y, x, conflicts))
-                    if self.queens[y] == row:
-                        tempConflicts += 1
-                #Check if queen in current column list is in the row we are checking, since it will count itself as a conflict in this case
-                if self.queens["Q" + str(col+1)] == row:
-                    tempConflicts -= 1
-                #Update the conflicts list for that particular block in the column
-                conflicts[x] = tempConflicts
-                
-                row += 1
-            col += 1
-
-    def checkConflictsRightDown(self):
-        col = 0
-
-        for conflicts in self.columns:
-
-            for x in range(len(conflicts)):
-                tempConflicts = 0
-                # print("x is: {}, conflicts is: {}".format(x, conflicts))
-                row = x + 1
-
-                for y in self.queens:
-                    if int(y[1]) - 1 > col:
-                        # print("Checking {}R:{} for {}R:{}".format(y, row, "Q" + str(col+1), x))
-                        if self.queens[y] == row:
-                            tempConflicts += 1
-                            # print("TEMPCONF+, current: {}".format(tempConflicts))
-
-                        row += 1
-
-                conflicts[x] = conflicts[x] + tempConflicts
-            col += 1
-
-    def checkConflictsRightUp(self):
-        col = 0
-
-        for conflicts in self.columns:
-
-            for x in range(len(conflicts)):
-                tempConflicts = 0
-
-                row = x - 1
-                for y in self.queens:
-                    if int(y[1]) - 1 > col:
-                        if self.queens[y] == row:
-                            tempConflicts += 1
-                        row -= 1
-
-                conflicts[x] = conflicts[x] + tempConflicts
-            col += 1
-
-    def checkConflictsLeftUp(self):
-        col = 0
-
-        for conflicts in self.columns:
-
-            for x in range(len(conflicts)):
-                tempConflicts = 0
-
-                row = x - col
-                for y in self.queens:
-                    if int(y[1]) - 1 < col:
-                        if self.queens[y] == row:
-                            tempConflicts += 1
-                        row += 1
-
-                conflicts[x] = conflicts[x] + tempConflicts
-            col += 1
-
-    def checkConflictsLeftDown(self):
-        col = 0
-
-        for conflicts in self.columns:
-
-            for x in range(len(conflicts)):
-                tempConflicts = 0
-
-                row = x + col
-                for y in self.queens:
-                    if int(y[1]) - 1 < col:
-                        if self.queens[y] == row:
-                            tempConflicts += 1
-                        row -= 1
-
-                conflicts[x] = conflicts[x] + tempConflicts
-            col += 1
+        start = timeit.default_timer()
+        colCounter = 0
+        for col in self.columns:
+            self.checkConflicts(col, colCounter)
+            colCounter += 1;
 
 
-    def checkConflicts(self):
-        
-        conflicts= 0;
-        self.checkConflictsHorizontal()
-        print("After Horizontal checks: ", end='')
-        print(self.columns)
-        self.checkConflictsRightDown()
-        print("After Down Right checks: ", end='')
-        print(self.columns)
-        self.checkConflictsRightUp()
-        print("After Up Right checks:    ", end='')
-        print(self.columns)
-        self.checkConflictsLeftUp()
-        print("After Left Up checks:    ", end='')
-        print(self.columns)
-        self.checkConflictsLeftDown()
-        print("After Left Down checks:    ", end='')
-        print(self.columns)
+        stop = timeit.default_timer()
 
-        #loop through index of colums for possible placements of queen
-        
-    #def moveQueen(self):
-        
-        
-    #def printBoard(self):
-    # print()
+        print('Time: ', stop - start)
 
-    
-        
-        
+    def checkConflicts(self, col, colCount):
+        y = colCount;  #Where we are in the columns
+        for position in col:
+            self.columns[colCount][position] = 0
+            nextTile = position
+
+            #columns is the integer that tells us where we are in self.columns
+            #This For loop checks for Diagonal Upper Right Corner
+            for columns in range(y, self.Max + 1):
+                columns += 1
+                nextTile -= 1
+                if(nextTile < 0 or columns > self.Max):
+                    break
+
+                if self.queens["Q" + str(columns)] == nextTile:
+                    self.columns[colCount][position] += 1
+
+            nextTile = position
+            # This For loop checks for Diagonal Bottom Right Corner
+            for columns in range(y, self.Max + 1):
+                columns += 1
+                nextTile += 1
+                if (nextTile > self.Max) or (columns > self.Max):
+                    break
+
+                if self.queens["Q" + str(columns)] == nextTile:
+                    self.columns[colCount][position] += 1
+
+            nextTile = position
+            # This For loop checks for Diagonal Upper Left Corner
+            for columns in range(y, -1, -1):
+                columns -= 1
+                nextTile -= 1
+                if (nextTile < 0) or (columns < 0):
+                    break
+                if self.queens["Q" + str(columns)] == nextTile:
+                    self.columns[colCount][position] += 1
+
+            nextTile = position
+            # This For loop checks for Diagonal Bottom Left Corner
+            for columns in range(y, -1, -1):
+                columns -= 1
+                nextTile += 1
+                if (nextTile > self.Max) or (columns < 0):
+                    break
+                if self.queens["Q" + str(columns)] == nextTile:
+                    self.columns[colCount][position] += 1
+
+            # This For loop checks for Horizontal
+            for columns in range(0, self.Max + 1):
+                print(columns)
+                if columns != colCount:
+                    if self.queens["Q" + str(columns)] == position:
+                        self.columns[colCount][position] += 1
+            #Check Diagonal
+
+
+
+
+
         
 
 
--- src/min-constraints.py
+++ src/min-constraints.py
@@ -3,9 +3,11 @@ from board import Board
 def main():
     n = int(input("How many Queens?"))
     chessBoard = Board(n)
+    #chessBoard.queens = {'Q0': 6, 'Q1': 0, 'Q2': 3, 'Q3': 5, 'Q4': 3, 'Q5': 4, 'Q6': 4, 'Q7': 2}
     print(chessBoard.queens)
+
+    chessBoard.updateConflicts()
     print(chessBoard.columns)
-    chessBoard.checkConflicts()
 
 
 
